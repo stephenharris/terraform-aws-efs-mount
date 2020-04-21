275b33f97db44f26cfb65be58a4e343469a6adc9
Stephen Harris <contact@stephenharris.info>
2020-04-21 18:22:42 +0100

    Fix syntax

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "file_system_dns_name" {
-  value = "${aws_efs_file_system.this.dns_name}"
+  value = aws_efs_file_system.this.dns_name
 }
 output "file_system_arn" {
-  value = "${aws_efs_file_system.this.arn}"
+  value = aws_efs_file_system.this.arn
 }
 
 output "ec2_security_group_id" {
