0b6721ada6ab82b68cd9fd5788c7900875294f5b
amarouane <amarouane@itsc.uah.edu>
2021-01-21 11:49:12 -0600

    creating a release v0.1.2

--- outputs.tf
+++ outputs.tf
@@ -9,8 +9,7 @@ output "ec2_security_group_id" {
   value = aws_security_group.mount_target_client.id
 }
 
-output "mount_target_id" {
+output "mount_target_ids" {
 
-  value =  {for mount_target in  aws_efs_mount_target.this:
-    mount_target.id => mount_target }
+  value =  [for mount_target in  aws_efs_mount_target.this: mount_target.id ]
 }
