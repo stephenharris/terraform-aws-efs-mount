c00b1258218000570e20c803078a0fa5d750ccae
gmautner <gilberto.mautner@locaweb.com.br>
2023-01-25 19:33:03 -0300

    add transition_to_ia var

--- inputs.tf
+++ inputs.tf
@@ -19,7 +19,6 @@ variable "encrypted" {
   type        = bool
 }
 
-
 variable "kms_key_id" {
   type        = string
   description = "The ARN of the key that you wish to use if encrypting at rest. If not supplied, uses service managed encryption. Can be specified only if `encrypted = true`"
@@ -36,6 +35,12 @@ variable "cidr_blocks" {
   type        = list
   description = "(Optional) A list of CIDR blocks to be allowed access to the mount points"
   default     = []
+}
+
+variable "transition_to_ia" {
+  description = "Indicates how long it takes to transition files to the IA storage class"
+  type        = string
+  default     = ""
 }
 
 variable "tags" {
